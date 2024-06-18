ea8035658826e0ab15cc655a5cb5aa1128986bf5
mikan-919 <131849133+mikan-919@users.noreply.github.com>
2024-06-18 17:52:50 +0900

    ADD@config.sh: check dir

--- install/config.sh
+++ install/config.sh
@@ -1,8 +1,24 @@
-cd `dirname $0`
+cd $(dirname $0)
+# ファイルを作成する関数
+makedir() {
+  local file_path="$1"
 
-function config_link(){
-rm -rf ~/$1
-ln -s $(realpath ../config/$1) $(realpath ~/$1)
+  # ディレクトリ部分を取得
+  local directory
+  directory=$(dirname "$file_path")
+
+  # ディレクトリが存在するか確認し、存在しない場合は作成
+  if [ ! -d "$directory" ]; then
+    mkdir -p "$directory"
+    echo "Create '$directory' dir "
+  fi
+}
+
+function config_link() {
+  TARGET=$(realpath ~/$1)
+  rm -rf $TARGET
+  makedir $TARGET
+  ln -s $(realpath ../config/$1) $TARGET
 }
 
 config_link .zshrc
